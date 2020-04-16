724ca62092334c759a719867dd1c6f79b7dff18f
Cyrill Frei <cfrei89@gmail.com>
2020-04-16 19:00:44 +0100

    Added ChatCollection class

--- models.py
+++ models.py
@@ -1,5 +1,45 @@
 from time import time
 
+class ChatCollection:
+    def __init__(self):
+        self.chats = []
+
+    def add_chat(self, chat):
+        """
+        Takes chat of type Chat and appends it to list of chats
+        """
+        self.chats.append(chat)
+
+    def chat_exists(self, name):
+        """
+        Takes string and returns True if the chatname already exists, False
+        otherwise
+        """
+        for chat in self.chats:
+            if name == chat.chat_name:
+                return True
+        return False
+
+    def get_chat_by_name(self, name):
+        """
+        Takes string and returns the chat with the matching name
+        """
+        if chat_exists(name):
+            return [chat for chat in chats if chat.chat_name == name][0]
+        else:
+            raise ValueError('A chat could not be found when it should have!')
+
+    def add_message(self, message_content, user, chat_name):
+        """
+        Takes the message content, the user and the chatname (all strings), Adds
+        a new message to the appropriate chat
+        """
+        if chat_exists(chat_name):
+            chat = get_chat_by_name(chat_name)
+            chat.add_message(Message(message_content, user))
+        else:
+            raise ValueError('A chat could not be found when it should have!')
+
 class Chat:
     def __init__(self, chat_name, user):
         self.chat_name = chat_name
