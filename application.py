131cad5224304cf5626fa8071d65d3b88e3dc2d4
Cyrill Frei <cfrei89@gmail.com>
2020-04-16 19:00:26 +0100

    Added ChatCollection class

--- application.py
+++ application.py
@@ -1,13 +1,60 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from models import *
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+chats = []
+
+if True:
+    chats.append(Chat("TestChat1", "Bob"))
+    chats[-1].add_message(Message("Oh wow, the first chat!", "Alice"))
+    chats[-1].add_message(Message("Blub", "Bob"))
+
+    chats.append(Chat("TestChat1", "Bob"))
+    chats[-1].add_message(Message("Oh wow, the first chat!", "Alice"))
+    chats[-1].add_message(Message("Blub", "Bob"))
+
+# It seems stupid to always send all chats with all messages, not necessary..
+# this could be just sending a "new message notification", the complete thing
+# is only loaded when the page is freshly loaded (F5)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    """
+    display the chat-page given in index.html
+    """
+    return render_template("index.html", chats=chats)
+
+@socketio.on("submit message")
+def send_message(message):
+    """
+    socket to receive new messages to be added
+    data should be json of shape:
+    {
+        "chat": "Chat 1",
+        "message": "Bla bla bla",
+        "author": "Bob"
+    }
+    Note that the timestamp is taken on the server side!
+    """
+    # update chats
+    add_to_dict = message["chat"]
+
+    # emit new_message to clients, handle this on client side
+    emit("new message", message, broadcast=True)
+
+@socketio.on("add chat")
+def send_message(chat):
+    """
+    socket to receive new chat to be added
+    data should be json of shape:
+    {
+        "chatname": "NewChat",
+        ""
+    }
+    """
